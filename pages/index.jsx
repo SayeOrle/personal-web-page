1d26a5c47e43cd510f70acf1b925663f8dc53741
Saye0rle <109271157+Saye0rle@users.noreply.github.com>
2022-11-29 20:14:59 +0300

    up

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,48 +10,114 @@ export default function Home() {
   const [tabs, setTabs] = useState(0);
 
   return (
-    <div onLoad={()=>setAnimation(true)} className="h-screen bg-[#131313] grid place-items-center text-white">
-      <div className="flex flex-col items-center text-center gap-y-6">
-        <div className={`flex flex-col items-center trnasition-all duration-[2s] relative -top-40 ${animation ? " opacity-100 visible -translate-y-0 slide-bottom "  :" opacity-0 invisible   "} `}>
+    <div
+      onLoad={() => setAnimation(true)}
+      className="h-screen bg-[#131313] grid place-items-center text-white"
+    >
+      <div className="div flex flex-col items-center text-center gap-y-6">
+        <div
+          className={`flex flex-col items-center  relative duration-[3s] -top-40 ${
+            animation
+              ? " opacity-100 visible slide-bottom "
+              : " opacity-0 invisible   "
+          } `}
+        >
           <div className="relative h-32 w-32 rounded-full overflow-hidden ">
             <Image src={"/prefil.gif"} fill />
           </div>
-          <h2>Saye Orle</h2>
-          <h3 className="text-color">Front-End Developer</h3>
+          <h2 className="text-2xl font-semibold">Saye Orle</h2>
+          <h3 className="text-color text-xs">Front-End Developer</h3>
         </div>
-        <div className="flex">
-          <div className="relative -top-[160px] slide-bottom">
+        <div className="flex text-sm gap-x-7">
+          <div
+            className={`relative -top-[160px] duration-[6s] slide-bottom ${
+              animation ? " opacity-100 visible " : " opacity-0 invisible "
+            }`}
+          >
             <h3>7</h3>
             <p>
               Years Of <br /> Work
             </p>
           </div>
-          <div className="relative -top-[160px] slide-bottom-200ms">
+          <div
+            className={`relative -top-[160px] duration-[6s] slide-bottom-150ms ${
+              animation ? " opacity-100 visible " : " opacity-0 invisible "
+            }`}
+          >
             <h3>7</h3>
             <p>
-              Completed
-              <br /> Projects
+              Years Of <br /> Work
             </p>
           </div>
-          <div className="relative -top-[160px] slide-bottom-300ms">
+          <div
+            className={`relative -top-[160px] duration-[6s] slide-bottom-200ms ${
+              animation ? " opacity-100 visible " : " opacity-0 invisible "
+            }`}
+          >
             <h3>7</h3>
             <p>
-              Cup Of
-              <br /> Tea
+              Years Of <br /> Work
             </p>
           </div>
         </div>
-        <div className="flex">
-          <button className="flex-1">GitHub</button>
-          <button className="">Discord</button>
-          <button className="">Linkedin</button>
+        <div
+          className={`flex flex-col gap-y-2 relative -top-40 duration-[8s] ${
+            animation
+              ? " opacity-100 visible slide-bottom-200ms "
+              : " opacity-0 invisible   "
+          } `}
+        >
+          <div>
+            <button className="hover:scale-105 transition-all flex-1 w-full">
+              GitHub
+            </button>
+          </div>
+          <div className="flex gap-x-4">
+            <button className="hover:scale-105 transition-all ">Discord</button>
+            <button className="hover:scale-105 transition-all ">
+              Linkedin
+            </button>
+          </div>
         </div>
-        <div className="flex gap-x-2">
-          <button onClick={() => setTabs(0)}>AboutMe</button>
-          <button onClick={() => setTabs(1)}>Skills</button>
-          <button onClick={() => setTabs(2)}>Projects</button>
+        <div
+          className={`flex gap-x-2 bg-[#212121] px-2 rounded-xl py-2  relative -top-40 duration-[10s] ${
+            animation
+              ? " opacity-100 visible slide-bottom-300ms "
+              : " opacity-0 invisible "
+          } `}
+        >
+          <button
+            className={`hover:bg-[#121212] transition-all ${
+              tabs == 0 ? " bg-[#121212] " : ""
+            }`}
+            onClick={() => setTabs(0)}
+          >
+            AboutMe
+          </button>
+          <button
+            className={`hover:bg-[#121212] transition-all ${
+              tabs == 1 ? " bg-[#121212] " : ""
+            }`}
+            onClick={() => setTabs(1)}
+          >
+            Skills
+          </button>
+          <button
+            className={`hover:bg-[#121212] transition-all ${
+              tabs == 2 ? " bg-[#121212] " : ""
+            }`}
+            onClick={() => setTabs(2)}
+          >
+            Projects
+          </button>
         </div>
-        <div>
+        <div
+          className={`relative -top-40 duration-[10s] ${
+            animation
+              ? " opacity-100 visible slide-bottom-400ms "
+              : " opacity-0 invisible "
+          }`}
+        >
           {tabs === 0 && <AboutMe />}
           {tabs === 1 && <Skills />}
           {tabs === 2 && <Projects />}
